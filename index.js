62712178971a3d367a4dacbee753fce00c36f4bd
Emily Iannizzi <eiannizzi18@gmail.com>
2023-04-18 21:38:52 -0400

    completed

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+var num1 = 62;
+var num2 = 1;
 
-// Write your code here
+var multiply = num1 * num2;
+
+var random = 1;
+
+Math.random();
+
+var num3 = 9;
+var num4 = 5;
+
+var mod = num3 % num4;
+
+var max = Math.max(5, 10, 15, 20);
